d96e0d6aba64d09433a4c89f1f67f6696b4c2389
Farheen <farheen201113@gmail.com>
2026-04-24 22:24:18 +0530

    fixed homepage route

--- urls.py
+++ urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from store import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls')),
     path('', views.home, name='home'),
     path('products', views.products_page, name='products'),
     path('login', views.login_page, name='login'),
